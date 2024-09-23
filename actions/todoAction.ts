05263f9fb6c66b3ee083fd591471f3108f97fa16
WalidElsergany74 <walidelsergany1@gmail.com>
2024-09-23 16:34:54 +0200

    first commit

---
+++ actions/todoAction.ts
@@ -0,0 +1,67 @@
+'use server'
+
+
+
+import { PrismaClient } from '@prisma/client'
+import { revalidatePath } from 'next/cache';
+
+const prisma = new PrismaClient()
+
+export const getTodoAction = async ({userId} : {userId : string | null}) => {
+   return await prisma.todo.findMany(
+    {
+      where : {
+       user_Id : userId as string
+      },
+      orderBy : {
+    createdAt : "desc"
+   }});
+};
+export const updateTodoAction = async ({id , title , body , completed} : {title : string , body? : string | undefined, id : string , completed : boolean}) => {
+ await prisma.todo.update({
+    where : {
+        id,
+    },
+    data : {
+        title, 
+        body,
+        completed,
+
+    }
+ })
+
+ revalidatePath("/")
+}
+export const createTodoAction = async ({
+    title,
+    body,
+    completed,
+    userId,
+  }: {
+    title: string,
+    body?: string | undefined,
+    completed: boolean,
+    userId: string | null,
+  }  )  => {
+    await prisma.todo.create({
+    data : {
+      title,
+      user_Id : userId as string ,
+        body,
+        completed,
+         
+    }
+    });
+  
+    revalidatePath("/");
+  };
+
+
+export const deleteTodoAction = async ({id} : {id : string}) => {
+    await prisma.todo.delete({
+        where : {
+            id,
+        }
+    })
+    revalidatePath("/")
+}
